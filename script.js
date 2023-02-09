8edef12c3c1591ea5e6717154f2671dce49de8d8
Adam Cox <adamc567@gmail.com>
2023-02-09 13:15:26 -0600

    Clean and refine JavaScript. Add final functions. Clean CSS

--- script.js
+++ script.js
@@ -1,64 +1,123 @@
-const oneBtn = document.querySelector('#one');
-const twoBtn = document.querySelector('#two');
-const threeBtn = document.querySelector('#three');
-const fourBtn = document.querySelector('#four');
-const fiveBtn = document.querySelector('#five');
-const sixBtn = document.querySelector('#six');
-const sevenBtn = document.querySelector('#seven');
-const eightBtn = document.querySelector('#eight');
-const nineBtn = document.querySelector('#nine');
-const zeroBtn = document.querySelector('#zero');
 const mainDisplay = document.querySelector('#calculatorMainDisplay')
 const subDisplay = document.querySelector('#calculatorSubDisplay');
 const allClearBtn = document.querySelector('#clear');
-const addBtn = document.querySelector('#add');
 const equalBtn = document.querySelector('#equal');
+const deleteBtn = document.querySelector('.delete');
+const numberBtn = document.querySelectorAll('.buttonNumber');
+const operatorBtn = document.querySelectorAll('.calculatorOperator');
+const deciBtn = document.querySelector('#deci');
+const sqrtBtn = document.querySelector('#squareRoot');
+let bananaInput = '';
+let valueReturn = '';
+let operator = ''
 
-let valueDisplay = [];
-let valueDisplayTwo = [];
-let addOn = false
-
-oneBtn.addEventListener('click', () => addToValue(oneBtn.value));
-twoBtn.addEventListener('click', () => addToValue(twoBtn.value));
-threeBtn.addEventListener('click', () => addToValue(threeBtn.value));
-fourBtn.addEventListener('click', () => addToValue(fourBtn.value));
-fiveBtn.addEventListener('click', () => addToValue(fiveBtn.value));
-sixBtn.addEventListener('click', () => addToValue(sixBtn.value));
-sevenBtn.addEventListener('click', () => addToValue(sevenBtn.value));
-eightBtn.addEventListener('click', () => addToValue(eightBtn.value));
-nineBtn.addEventListener('click', () => addToValue(nineBtn.value));
-zeroBtn.addEventListener('click', () => addToValue(zeroBtn.value));
-
-equalBtn.addEventListener('click', () => equal())
-addBtn.addEventListener('click', () => add())
-allClearBtn.addEventListener('click', () => valueDisplay = []);
-allClearBtn.addEventListener('click', () => mainDisplay.innerHTML = 0);
-
-function addToValue(a) {
-    mainDisplay.innerHTML = '';
-    valueDisplay.push(a)
-    mainDisplay.innerHTML = (valueDisplay.join(''));
-    return (valueDisplay.join(''))
+sqrtBtn.addEventListener('click', () => sqRoot(bananaInput))
+numberBtn.forEach((button) =>
+button.addEventListener('click', () => newInput(button.value)));
+operatorBtn.forEach((button) =>
+button.addEventListener('click', () => setOperator(button.textContent)));
+operatorBtn.forEach((button) =>
+button.addEventListener('click', newValue));
+window.addEventListener('keydown', (e) => keyboard(e));
+equalBtn.addEventListener('click', () => operate(operator))
+deleteBtn.addEventListener('click', deleteLast)
+allClearBtn.addEventListener('click', clearAll)
+window.addEventListener('keydown', (e) => console.log(e.key));
+deciBtn.addEventListener('click', addDot);
+function newInput (a) {
+bananaInput += a;
+mainDisplay.innerHTML = bananaInput;
 }
-
-function add() {
-    addOn = true
-    valueDisplayTwo = (valueDisplay.join(''));
-    subDisplay.innerHTML = valueDisplayTwo;
-    valueDisplay = [];
-    console.log(addOn);
-
+function keyboard (e){
+    if (e.key >= 0 && e.key <= 9) {
+    newInput(String(e.key))
+} else if (e.key === '+' || e.key === '-' || e.key === '/' || e.key === '*'){
+    setOperator(e.key)
+    newValue();
+} else if (e.key === 'Enter' || e.key === '=') {
+    operate(operator);
+} else if (e.key === 'Backspace') {
+    deleteLast();
+} else if (e.key === '.') {
+    addDot();
 }
-
-function equal() {
-let result
-result = valueDisplay.map(Number) + valueDisplayTwo.map(Number);
-mainDisplay.innerHTML = result;
-console.log(result)
+}
+function clearAll() {
+    mainDisplay.textContent = '0'
+    subDisplay.textContent = '0';
+    bananaInput = ''
+    valueReturn = ''
+}
+function deleteLast() {
+    bananaInput = bananaInput.toString();
+    bananaInput = bananaInput.substring(0, bananaInput.length-1);
+    mainDisplay.textContent = (bananaInput);
+}
+function addDot() {
+    if (bananaInput === '') {
+        bananaInput = '0';
+    } else if (mainDisplay.textContent.includes('.')) {
+        return
     }
+    bananaInput += '.'
+    mainDisplay.textContent = bananaInput;
+}
+function newValue (a) {
+    valueReturn = bananaInput;
+    bananaInput = '';
+    mainDisplay.innerHTML = 0;
+    subDisplay.innerHTML = `${valueReturn} ${operator}`;
+}
+function setOperator (a){
+   return operator = a; 
+}
+function add(a, b) {
+    bananaInput = roundNumber(a + b);
+    mainDisplay.textContent = bananaInput;
+    subDisplay.textContent = `${b} ${operator} ${a} =`
+}
+function minus(a, b) {
+    bananaInput = roundNumber(b - a);
+    mainDisplay.textContent = bananaInput;
+    subDisplay.textContent = `${b} ${operator} ${a} =`
+}
+function multiply(a, b) {
+    bananaInput = roundNumber(a * b);
+    mainDisplay.textContent = bananaInput;
+    subDisplay.textContent = `${b} ${operator} ${a} =`
+}
+function divide(a, b) {
+    bananaInput = roundNumber(b / a);
+    mainDisplay.textContent = bananaInput;
+    subDisplay.textContent = `${b} ${operator} ${a} =`
+}
+function sqRoot(a) {
+    bananaInput = roundNumber(Math.sqrt(a))
+    mainDisplay.textContent = bananaInput;
 
-// define a string
-// have string take in value when button pressed,
-// create function to manipulate string
-// display string
-// display result of function
+}
+function operate(operator) {
+    let aValue = Number(bananaInput);
+    let bValue = Number(valueReturn);
+    switch (operator)
+        {
+            case '+':
+                return add(aValue, bValue);
+            case '-':
+                return minus(aValue, bValue);
+            case '*':
+                return multiply(aValue, bValue);
+            case '/':
+                if (aValue === 0){
+                    alert(`You cannot devide by 0! You're mad!`)
+                } else {
+                return divide(aValue, bValue);
+                }
+            default:
+                console.log('dog')
+                return null;
+        }
+}
+function roundNumber (number) {
+    return Math.round(number * 1000) / 1000;
+}
